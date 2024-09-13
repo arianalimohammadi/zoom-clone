9f7d6c000e978269da9fd4a57e0dd145dfe29e6c
Arian M <arianalimohammadi>
2024-09-12 21:10:20 -0700

    sign up page update

--- app/(auth)/sign-in/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-in/sign-up/[[...sign-up]]/page.tsx
@@ -1,12 +1,9 @@
-import { SignUp } from '@clerk/nextjs'
-import React from 'react'
+import { SignUp } from '@clerk/nextjs';
 
-const SignUpPage = () => {
+export default function SignUpPage() {
   return (
     <main className="flex h-screen w-full items-center justify-center">
-        <SignUp />
+      <SignUp />
     </main>
-  )
+  );
 }
-
-export default SignUpPage
